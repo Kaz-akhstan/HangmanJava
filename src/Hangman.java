d2df818694ebfa06e208d63ea797d9986a609b4a
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-02-01 14:59:19 +0100

    Commit

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
+        /*Hämta slumpat ord från fil*/
         String inWord = "hangman";
         char[] word = new char[inWord.length()];
         char[] answer = new char[inWord.length()];
@@ -12,26 +13,49 @@ public class Hangman {
         }
         Scanner sc = new Scanner(System.in);
         int difficulty = sc.nextInt();
-        mainGame(word, answer, difficulty);
+        boolean playingGame = true;//Hitta bättre lösning
+        mainGame(word, answer, difficulty, playingGame);
     }
-    static void mainGame(char[] word, char[] answer, int difficulty)
+    static void mainGame(char[] word, char[] answer, int difficulty, boolean playingGame)
     {
+        Scanner sc = new Scanner(System.in);
         while (true)
         {
-            Scanner sc = new Scanner(System.in);
+            if(!playingGame)
+            {
+                break;
+            }
+            boolean charFound = false;
             char guess = sc.nextLine().charAt(0);
             for(int i = 0; i < word.length; i++)
             {
                 if(guess == word[i])
                 {
                     answer[i] = guess;
+                    charFound = true;
                 }
-                else
-                {
-                    difficulty--;
-                }
+            }
+            if(!charFound)
+            {
+                loseLife(difficulty, playingGame);
             }
             System.out.println(answer);
         }
+    }
+    static int loseLife(int difficulty, boolean playingGame)
+    {
+        Scanner sc = new Scanner(System.in);
+        difficulty--;
+        System.out.println(difficulty);
+        if(difficulty <= 0)
+        {
+            System.out.println("GAME OVER!\nPLAY AGAIN?\nY/N");
+            if(sc.nextLine().equals("Y"))
+            {
+                playingGame = false;//Hitta bättre lösning
+            }
+            /*Tillbaka till start menyn*/
+        }
+        return difficulty--; //Returnar inte
     }
 }
