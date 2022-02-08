463dfab092b50603fe606b5bea0af0aac63e7c5d
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-02-08 21:30:02 +0100

    Färdig

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,18 +1,20 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         gameStarter();
     }
-    static void gameStarter()
-    {
-        /*  -!To Do!-
-        *   Slumpade ord
-        *   Win-condition
-        *   Bättre UI
-        */
-        String inWord = "hangman";
+    static void gameStarter() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("wordList.txt"));
+        Random r = new Random();
+        for (int i = 0; i<r.nextInt(851); i++)
+        {
+            br.readLine();
+        }
+        String inWord = br.readLine();
         char[] word = new char[inWord.length()];
         char[] answer = new char[inWord.length()];
         for(int i = 0; i < inWord.length(); i++)
@@ -20,16 +22,21 @@ public class Hangman {
             word[i] = inWord.charAt(i);
             answer[i] = '-';
         }
-        Scanner sc = new Scanner(System.in);
         int difficulty = 7;
         System.out.println(answer);
         mainGame(word, answer, difficulty);
     }
-    static void mainGame(char[] word, char[] answer, int difficulty)
-    {
+    static void mainGame(char[] word, char[] answer, int difficulty) throws IOException {
         Scanner sc = new Scanner(System.in);
         while (true)
         {
+            if(Arrays.equals(word, answer))
+            {
+                System.out.println("The word: " + Arrays.toString(answer).replaceAll(", ", "") + " has been revealed! You Win!");
+                System.out.println("Your score was " + difficulty);
+                gameOver();
+                break;
+            }
             boolean charFound = false;
             char guess = sc.next().toLowerCase().charAt(0);
             for(int i = 0; i < word.length; i++)
@@ -43,53 +50,51 @@ public class Hangman {
             if(!charFound)
             {
                 difficulty--;
-                loseLife(difficulty);
+                drawImage(difficulty, word);
             }
             System.out.println(answer);
         }
     }
-    static void loseLife(int difficulty)
-    {
-        Scanner sc = new Scanner(System.in);
+    static void drawImage(int difficulty, char[] word) throws IOException {
         System.out.println(difficulty);
-        String line;
-        File file = new File("man-copy.txt");
-        int count = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))  //Fungerar inte just nu, fixa
+        switch (difficulty)
         {
-            while (br.readLine() != null)
-            {
-                line = br.readLine();
-                if(line.equals(Integer.toString(difficulty)))
-                {
-                    for (int j=0; j<8; j++)
-                    {
-                        line = br.readLine();
-                        System.out.println(line);
-                    }
-                }
-                else{
-                    br.readLine();
-                }
-            }
-            //line = "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========\n";
+            case 6:
+                System.out.println("  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========\n");
+                break;
+            case 5:
+                System.out.println("  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========\n");
+                break;
+            case 4:
+                System.out.println("  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========\n");
+                break;
+            case 3:
+                System.out.println("  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========\n");
+                break;
+            case 2:
+                System.out.println("  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========\n");
+                break;
+            case 1:
+                System.out.println("  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========\n");
+                break;
+            case 0:
+                System.out.println("  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========\n");
+                System.out.println("The word was: " + Arrays.toString(word).replaceAll(", ", ""));
+                System.out.println("GAME OVER!");
+                gameOver();
         }
-        catch (IOException e)
+    }
+    static void gameOver() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("PLAY AGAIN?\nY/N");
+        if(sc.next().equalsIgnoreCase("Y"))
         {
-            System.out.println(e);
+            gameStarter();
         }
-        if(difficulty <= 0)
-        {
-            System.out.println("GAME OVER!\nPLAY AGAIN?\nY/N");
-            if(sc.next().toUpperCase().equals("Y"))
-            {
-                gameStarter();
-            }
-            else {
-                System.out.println("Thanks For Playing!");
-                sc.next();
-                System.exit(1);
-            }
+        else {
+            System.out.println("Thanks For Playing!");
+            sc.next();
+            System.exit(0);
         }
     }
 }
